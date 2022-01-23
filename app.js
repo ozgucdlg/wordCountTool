3b441d4b401cc5e4ea0bbe1ca51854651eb24562
DALGA Ozguc <ozgucdalga@gmail.com>
2022-01-23 00:10:27 -0500

    completion

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-let button = document.createElement('btn');
+
+// we connected button and btn  with first code  => button in backend, btn in userinterface
+let button = document.getElementById('btn');
+
+// when the click  to the button;
 button.addEventListener('click', function(){
+    // we will take the string and and will get the value with word variable
     let word = document.getElementById('str').value;
     let count = word.length;
     let outputDiv = document.getElementById('output');
+
     outputDiv.innerHTML = `<h1> ${count}</h1>`
-})
+});
